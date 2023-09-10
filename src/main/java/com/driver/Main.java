0aae773b677f6627a238563d8ba42565bf8fd27d
=vamsi <=>
2023-09-10 19:45:37 +0530

    comited

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        RWOnly rwOnly=new RWOnly();
+        System.out.println("Befor setting name "+ rwOnly.getName());
+        rwOnly.setName("vamsi");
+        System.out.println(rwOnly.getName());
+    }
 }
